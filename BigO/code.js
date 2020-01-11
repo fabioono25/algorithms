e931583b7c00e1d874ce999a6fb07bd65f20ae1f
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-01-11 22:07:47 +0000

    first commit

---
+++ BigO/code.js
@@ -0,0 +1,11 @@
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 'nemo') {
+            console.log(' found Nemo! ');
+        }
+    }
+}
+
+findNemo(nemo);
