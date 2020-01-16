e87f8c189dc937061c8b64dce7a5a9393db6164d
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-01-16 07:42:40 +0000

    Big(O) rules

--- BigO/Rules.js
+++ BigO/Rules.js
@@ -18,4 +18,48 @@ function findNemo(array) {
     }
 }
 
-findNemo(largeArray); //O(n) - worst scenario
+//findNemo(largeArray); //O(n) - worst scenario
+
+//SECOND RULE: drop the constants = we don't care about n/2 or 100 
+
+printFirstItemThenFirstHalfThenSayHi100Times(items) {
+    console.log(items[0]); //O(1)
+
+    var middleIndex = Math.floor(items.length / 2); //O(n/2) -> O(n)
+    var index = 0;
+
+    while (index < middleIndex) {
+        console.log(items[index]);
+        index++;
+    }
+
+    for (var i = 0; i < 100; i++) { //O(100) - O(1)
+        console.log('hi');
+    }
+}
+
+//result -> O(n + 1) = O(n)
+
+
+//THIRD RULE: different terms for inputs
+function compressBoxesTwice(boxes) {
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+
+    boxes.forEach(function(boxes){
+        console.log(boxes);
+    });
+}
+
+//O(2n) -> O(n)
+
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(function(boxes){  //O(a)
+        console.log(boxes);
+    });
+
+    boxes2.forEach(function(boxes){ //O(b)
+        console.log(boxes);
+    });
+} //different terms for inputs -> O(a + b)
