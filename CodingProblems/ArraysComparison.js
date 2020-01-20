89526f8be6dc0101a67696530aaad655fe95e28f
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-01-20 10:24:29 +0000

    better solutions for array comparison

--- CodingProblems/ArraysComparison.js
+++ CodingProblems/ArraysComparison.js
@@ -34,16 +34,49 @@ function comparingArrays(array1, array2) { //O(n^2)
     //array1 => object {a:true, b:true, c:true, x:true}
 }
 
-function containsCommonItem(array1, array2){
-    //loop first array and create properties === items in the array - O(n)
+// comparingArrays(['a','b','z'],['c','d']);
+// comparingArrays(['a','b','z'],['c','a']);
 
+//better solution
+//we must consider divide the two loops in two different methods
+//another thing to consider is verify the null values
+//we must add better names for the variables and verify the size complexity (in this case is bigger)
+function containsCommonItem(array1, array2){
+
+    if (array1 == null || array2 == null){
+        console.log('error');
+        return false;
+    }
+
+    //loop first array and create properties === items in the array - O(n)
+    let map = {};
+    for (let i = 0; i < array1.length; i++) {
+        const item = array1[i];
+        map[item] = true;
+    }
 
     //loop through second array and check if item in second array exists on created object - O(n)
+    for (let j = 0; j < array2.length; j++) {
+        if (map[array2[j]]) {
+            console.log(true);
+            return true;
+        }
+    }
+
+    console.log(false);
+} 
+//O(a + b) - Time Complexity
+//O(a) - Space Complexity
+
+containsCommonItem(['a','b','z'],['c','d']);
+containsCommonItem(['a','b','z'],['c','a']);
+containsCommonItem(['a','b','z']);
 
 
-} //O(a + b)
+//using specific language methods
+function containsCommonItems2(array1, array2) {
+    return array1.some(item => array2.includes(item));
+}
 
-
-comparingArrays(['a','b','z'],['c','d']);
-comparingArrays(['a','b','z'],['c','a']);
-
+console.log(containsCommonItems2(['a','b','z'],['c','d']));
+console.log(containsCommonItems2(['a','b','z'],['c','a']));
