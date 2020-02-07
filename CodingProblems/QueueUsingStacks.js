fc33ad3163f4f548aa921e83b55b2c72561926c3
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-02-07 09:16:54 +0000

    queues and stacks

---
+++ CodingProblems/QueueUsingStacks.js
@@ -0,0 +1,91 @@
+/**
+ * Implement the following operations of a queue using stacks
+ * push(x) -- Push element x to the back of queue.
+ * pop() -- Removes the element from in front of queue.
+ * peek() -- Get the front element.
+ * empty() -- Return whether the queue is empty.
+ * MyQueue queue = new MyQueue();
+ * queue.push(1);
+ * queue.push(2);  
+ * queue.peek();  // returns 1
+ * queue.pop();   // returns 1
+ * queue.empty(); // returns false
+ 
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+
+class MyQueue {
+    /** Initialize your data structure here. */
+    constructor() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    push(value) {
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    pop() {
+        
+    }
+    
+    /** Get the front element. */
+    peek() {
+        
+    }
+    
+    /** Returns whether the queue is empty. */
+    empty() {
+        
+    }
+};
+
+/**
+ * class CrazyQueue {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+    return this.first[this.first.length - 1];
+  }
+}
+
+const myQueue = new CrazyQueue();
+myQueue.peek();
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+myQueue.peek();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.peek();
+ */
---
+++ DataStructures/StacksWithArrays.js
@@ -0,0 +1,39 @@
+//Using Linked List
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.array = [];
+    }
+
+    peek() {
+        return this.array[this.array.length-1];
+    }
+
+    push(value) {
+        this.array.push(value);
+        return this;
+    }
+
+    pop() {
+        this.array.pop();
+        return this;
+    }
+
+    //isEmpty
+}
+
+const myStack = new Stack();
+myStack.push('A');
+myStack.push('B');
+myStack.push('C');
+myStack.peek();
+myStack.pop();
+myStack.pop();
+myStack.peek();
