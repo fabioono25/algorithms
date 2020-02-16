ffb22ac6d54fdf0b7b92063ac60bdbfc2fead189
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-02-16 10:08:26 +0000

    Sorting Exercises

---
+++ CodingProblems/WithSortingAlgorithmsUsing.js
@@ -0,0 +1,40 @@
+//#1 - Sort 10 schools around your house by distance:
+
+//it's small and simple - Intertion Sort - easy to code - space complexity O(1)
+
+
+
+//#2 - eBay sorts listings by the current Bid amount:
+
+//Radix or Counting sort (it's a number to 1 - 1000) - fixed length of integers
+
+
+//#3 - Sport scores on ESPN
+
+//quick sort: most efficient - scores mostly sorted - for merge sort can be too much
+
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+
+//merge sort: we're not sorting in memory - worried about performance - O(n log n)
+
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+
+//Insertion sort: assuming that mostly of previous data is already sorted - for preorder is good
+
+
+//#6 - Temperature Records for the past 50 years in Canada
+
+//Radix or Counting if no decimals places
+//otherwise QuickSort
+
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+
+//Merge Sort or Quick Sort (if not too worried about worst case - don't forget to pick a good pivot)
+
+
+//#8 - You want to teach sorting for the first time
+
+//Bubble sort and selection sort
