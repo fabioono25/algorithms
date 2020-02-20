67f6009001bf2fc55d55530ad73d37b4296b9922
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-02-20 22:22:28 +0000

    Fibonacci with bottom-up approach

--- CodingProblems/Fibonacci.js
+++ CodingProblems/Fibonacci.js
@@ -51,3 +51,12 @@ function fibonacciWithCache() {
 const fasterFib = fibonacciWithCache();
 fasterFib(10);
 
+//dynamic 2:
+function fiboDynamicV2(n){ //bottom-up approach
+    let answer = [0, 1];
+    for (let i = 2; i < n; i++) {
+        answer.push(answer[i-2] + answer[i-1]);
+    }
+    return answer.pop();
+}
+
