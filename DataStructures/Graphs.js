488458a070bbc6091e9f96dd1010adc899937041
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-16 21:54:05 +0000

    graphs

--- DataStructures/Graphs.js
+++ DataStructures/Graphs.js
@@ -1,3 +1,17 @@
+//edge list
+const graph = [[0,2],[2,3],[2,1],[1,3]];
+
+//adjacent list
+const graph = [[2],[2,3],[0,1,3],[1,2]];
+
+//adjacent matrix
+const graph = [
+  [0, 0, 1, 0],
+  [0, 0, 1, 1],
+  [1, 1, 0, 1],
+  [0, 1, 1, 0]
+]
+
 class Graph { 
     
     constructor() { 
