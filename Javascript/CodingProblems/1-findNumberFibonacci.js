b4780fad7b4935c1a2ec7e88e21a4f1ae1b07407
Fabio Ono Tavoloni <fabioono25@gmail.com>
2020-03-25 12:08:04 +0000

    Fibonacci with recursion

--- Javascript/CodingProblems/1-findNumberFibonacci.js
+++ Javascript/CodingProblems/1-findNumberFibonacci.js
@@ -1,36 +1,52 @@
-//given a position, return the number in a Fibonacci series
-//e.g. - What’s the number in position 6, in a Fibonacci series? R = 8. 
+//given a n, return the number in a Fibonacci series
+//e.g. - What’s the number in n 6, in a Fibonacci series? R = 8. 
 
 
 //Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21
-//position          0  1  2  3  4  5  6   7  8
-function findNumberFibonacci(position) {
+//n          0  1  2  3  4  5  6   7  8
+function findNumberFibonacci(n) {
 
-    if (position < 2) return position;
+    if (n < 0) return 0;
 
-    let currentPosition = 2;
+    if (n < 2) return n;
+
+    let currentn = 2;
     
     let previousValue = 0;
     let lastValue = 1;
 
-    while (currentPosition <= position) {
+    while (currentn <= n) {
         const temp = lastValue;
         lastValue = lastValue + previousValue;
         previousValue = temp;
-        currentPosition++;
+        currentn++;
     }
 
     return lastValue;
 } //O(n)
 
-console.log(`Position 0: ${findNumberFibonacci(0)}.`);
-console.log(`Position 1: ${findNumberFibonacci(1)}.`);
-console.log(`Position 2: ${findNumberFibonacci(2)}.`);
-console.log(`Position 3: ${findNumberFibonacci(3)}.`);
-console.log(`Position 4: ${findNumberFibonacci(4)}.`);
-console.log(`Position 5: ${findNumberFibonacci(5)}.`);
-console.log(`Position 6: ${findNumberFibonacci(6)}.`);
-console.log(`Position 7: ${findNumberFibonacci(7)}.`);
-console.log(`Position 8: ${findNumberFibonacci(8)}.`);
-console.log(`Position 40: ${findNumberFibonacci(40)}.`);
-console.log(`Position 80: ${findNumberFibonacci(80)}.`);
+//we can solve in a recursive way
+function findNumberFibonacciRecursion(n) {
+    if (n <= 0) {
+        return 0
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return findNumberFibonacciRecursion(n - 1) + findNumberFibonacciRecursion(n - 2);
+    }
+}
+
+console.log(`n 0: ${findNumberFibonacci(0)}.`);
+console.log(`n 1: ${findNumberFibonacci(1)}.`);
+console.log(`n 2: ${findNumberFibonacci(2)}.`);
+console.log(`n 3: ${findNumberFibonacci(3)}.`);
+console.log(`n 4: ${findNumberFibonacci(4)}.`);
+console.log(`n 5: ${findNumberFibonacci(5)}.`);
+console.log(`n 6: ${findNumberFibonacci(6)}.`);
+console.log(`n 7: ${findNumberFibonacci(7)}.`);
+console.log(`n 8: ${findNumberFibonacci(8)}.`);
+console.log(`n 40: ${findNumberFibonacci(40)}.`);
+console.log(`n 80: ${findNumberFibonacci(80)}.`);
+
+console.log(`n 8 with recursion: ${findNumberFibonacciRecursion(8)}.`);
+console.log(`n 40 with recursion: ${findNumberFibonacciRecursion(40)}.`);
