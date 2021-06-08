4070ced638c97a97522f940d68d5e50250e018cb
fabioono25 <fabioono25@gmail.com>
2021-06-08 11:07:09 +0100

    working with arrays

--- Javascript/DataStructures/Arrays.js
+++ Javascript/DataStructures/Arrays.js
@@ -2,7 +2,7 @@
 
 const strings = ['a','b','c','d']; //positions in memory 0, 1, 2, 3
 
-//4 items - 32 bits -> 4 * 4 = 16 bytes of storage
+//4 items - 32 bits -> 4 items * 4 bytes each item = 16 bytes of storage
 
 //console.log(strings[2]);
 
@@ -14,15 +14,15 @@ strings.push('e'); //O(1)
 //pop
 strings.pop(); //O(1)
 
-console.log(strings);
+//console.log(strings);
 
 //unshift (add in the beginning)
 strings.unshift('x'); //O(n) - all items are reorganized in memory - not the best strategy to add items
 
-//splice (add in the middle)
+//splice (add in the middle) - same of previous for the next items
 strings.splice(2, 0, 'z'); //O(n/2) => O(n)
 
-console.log(strings);
+//console.log(strings);
 
 
 //static arrays: C++
@@ -30,3 +30,49 @@ console.log(strings);
 
 //in Javascript/Python, the arrays are automatically allocate memory according the increase resize
 //with we add a new letter to initial strings array, it usually doubles the space (it'll copy the entire array)
+
+class MyArray {
+    constructor() {
+        this.length = 0;
+        this.data = {};
+    }
+
+    get(index) {
+        return this.data[index];
+    }
+
+    push(item) {
+        this.data[this.length] = item;
+        this.length++;
+        return this.length;
+    }
+
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index) {
+      const item = this.data[index];
+      this._shiftItems(index);
+      return item;
+    }
+
+    _shiftItems(index) {
+      for (let i = index; i < this.length; i++) {
+        this.data[i] = this.data[i + 1];
+      }
+      delete this.data[this.length - 1]; // very last item treatment
+      this.length--;
+    }
+}
+
+const newArray = new MyArray();
+console.log(newArray.get(0));
+newArray.push(1);
+newArray.push(2);
+newArray.push(3);
+newArray.pop();
+console.log(newArray.get(0));
