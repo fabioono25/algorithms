1b936564f65364c090e228402ea5549f5387ee83
fabioono25 <fabioono25@gmail.com>
2022-04-24 10:04:06 +0200

    bootstrapping models to be used

---
+++ CSharp/Algorithms/CodeChallenges/Ex2.cs
@@ -0,0 +1,19 @@
+namespace Algorithms.CodeChallenges
+{
+    /***
+    * verify if a string is an anagram or not
+    * anagram and nararam are anagrams
+    * rat and car are not anagrams
+    ***/
+    public static class Ex2
+    {
+        public static void Execute(){
+            
+        }
+
+        // considering that the input strings are same length, otherwise we should verify and return false if they are different
+        private static void method() {
+            
+        }
+    }
+}
