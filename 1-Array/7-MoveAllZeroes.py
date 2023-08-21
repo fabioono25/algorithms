ae5859a59919a6006b966acfbf9c9895b7379755
fabioono25 <fabioono25@gmail.com>
2023-08-21 19:59:55 +0200

    reorganize examples

---
+++ 1-Array/7-MoveAllZeroes.py
@@ -0,0 +1,8 @@
+# Given an array of random numbers, Push all the zero’s of a given array to the end of the array.
+# Input: arr[]={1, 2, 0, 4, 3, 0, 5, 0} Output: arr[]={1, 2, 4, 3, 5, 0, 0, 0}
+
+# Simple solution. Time Complexity: O(n) Space Complexity: O(1)
+
+
+# Partitioning the Array
+
---
+++ 1-Array/8-RearrangeTwoPoiner.py
@@ -0,0 +1,2 @@
+# Rearrange an array in maximum minimum form using Two Pointer Technique
+# example: Input : arr[] = {1, 2, 3, 4, 5, 6, 7} Output : arr[] = {7, 1, 6, 2, 5, 3, 4}
